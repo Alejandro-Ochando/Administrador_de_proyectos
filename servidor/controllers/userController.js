30770c921f1ede64f5f9d881907ea79be4c8e614
Alejandro-Ochando <alejandro.ochando@live.com>
2021-02-20 22:10:01 +0100

    Agregando el controlador para agregar usuarios

---
+++ servidor/controllers/userController.js
@@ -0,0 +1,5 @@
+const chalk = require('chalk');
+
+exports.createUser = (req, res) => {
+    console.log(chalk.cyan('Desde createuser'));
+};
--- servidor/routes/users.js
+++ servidor/routes/users.js
@@ -1,11 +1,11 @@
 //Rutas para crear usuarios
 const express = require('express');
 const router = express.Router();
+const userController = require('../controllers/userController');
+
 
 // Crea un usuario
 // api/users
-router.post('/', () => {
-
-});
+router.post('/', userController.createUser );
 
 module.exports = router;
