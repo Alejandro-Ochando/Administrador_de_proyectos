98629dbb7dbdf80d312f416464918fccf4b01767
Alejandro-Ochando <alejandro.ochando@live.com>
2021-02-20 14:42:31 +0100

    Creando el servidor Express

--- servidor/index.js
+++ servidor/index.js
@@ -1,1 +1,20 @@
-console.log('desde index.js');
+const express = require('express');
+
+// Crear el servidor
+const app = express();
+
+// Puerto de la app
+const PORT = process.env.PORT || 4000;
+
+// Definir la pagina principal
+// Probando si funciona el server.
+/*
+app.get('/', (req, res) => {
+    res.send('Hola Mundo')
+});
+*/
+
+// Arranque
+app.listen(PORT, () => {
+    console.log('El servidor esta funcionando en el puerto ', PORT);
+});
