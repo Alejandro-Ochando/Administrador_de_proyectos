dece942b019c54d4fc8c4a462e5b7d4285c29279
Alejandro-Ochando <alejandro.ochando@live.com>
2021-03-02 17:44:34 +0100

    Insertando nuevos proyetos

--- servidor/controllers/projectController.js
+++ servidor/controllers/projectController.js
@@ -1,3 +1,15 @@
+const Project = require('../models/Project');
+
 exports.createProject = async (req, res ) => {
-    //Dar funcionalidad
+    
+    try {
+        //Crear un nuevo proyecto
+        const project = new Project(req.body);
+        await project.save();
+        res.json(project);
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Hubo un error');
+    };
 };
