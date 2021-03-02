37b8bdc76961dc73019d3f5059b701ce223328ff
Alejandro-Ochando <alejandro.ochando@live.com>
2021-03-02 19:04:16 +0100

    Quitando código sobrante

--- servidor/middleware/auth.js
+++ servidor/middleware/auth.js
@@ -3,7 +3,6 @@ const jwt = require('jsonwebtoken');
 module.exports = function(req, res, next) {
     // Leer el token del header
     const token = req.header('x-auth-token');
-    console.log(token);
 
     // Revisar si no hay token
     if(!token) {
@@ -12,10 +11,8 @@ module.exports = function(req, res, next) {
 
     // Validar el token
     try {
-        const cifrado = jwt.verify(token, process.env.SECRET);
-        console.log(cifrado.user);
-        req.user = cifrado.user;
-        console.log(req.user);
+        const encrypt = jwt.verify(token, process.env.SECRET);
+        req.user = encrypt.user;
         next();
     } catch (error) {
         res.status(401).json({ msg: "Token no valido" });
